3788e74814193b517ba8a284e705dd973536fa91
Mestima <mestima@nilored.ru>
2022-05-10 17:54:13 +0300

    Update modinfo.lua

--- data/modinfo.lua
+++ data/modinfo.lua
@@ -1,7 +1,7 @@
-name = "Reliable Wanda [dev]"
+name = "Reliable Wanda"
 description = "A bundle of modifications that makes Wanda more reliable"
 author = "Mestima"
-version = "1.2.2"
+version = "1.2.3"
 forumthread = ""
 api_version = 6
 api_version_dst = 10
